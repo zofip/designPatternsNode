d708452e714a5a9e4a6430eaf12ea7c41caef2ff
zofi <zofi.src@gmail.com>
2020-01-29 11:00:43 -0500

    [FEAT] complete clone shopper

--- prototypePattern/Shopper.js
+++ prototypePattern/Shopper.js
@@ -1,10 +1,33 @@
-var logger = require('./Logger');
-
 class Shopper {
-    constructor(name, money=0) {
+    constructor(name) {
         this.name = name;
-        this.money = money;
-        logger.log(`New Shopper: ${name} has ${money} in their account.`);
+        this._shoppingList = [];
+    }
+
+    set name(value){
+        this._name = value;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    get shoppingList() {
+        return this._shoppingList.join(', ');
+    }
+
+    addItemToList(item) {
+        this._shoppingList.push(item);
+    }
+
+    clone() {
+        var proto = Object.getPrototypeOf(this);
+        var cloned = Object.create(proto);
+
+        cloned._name = this._name;
+        cloned._shoppingList = [...this._shoppingList];
+
+        return cloned;
     }
 }
 
